a73faaec44eaf6a3dee08bac52ca94993c8ae5fb
Joseph_Oderoh <joseph.oderoh@student.moringaschool.com>
2022-06-11 13:32:34 +0300

    st:add project profile models

--- awards/models.py
+++ awards/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 # Create your models here.
 
 
@@ -43,4 +45,25 @@ class Project(models.Model):
   
 
     def __str__(self):
-        return self.user.username       
+        return self.title      
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    profile_photo = CloudinaryField('image')
+    bio = models.TextField(max_length=500, default="Your Bio", blank=True)
+    name = models.CharField(blank=True, max_length=120)
+    location = models.CharField(max_length=60, blank=True)
+    contact = models.EmailField(max_length=100, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()

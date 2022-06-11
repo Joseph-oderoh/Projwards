bae58eb97cea37d74c6a1b0323acc5d07e9f1dfd
Joseph_Oderoh <joseph.oderoh@student.moringaschool.com>
2022-06-12 00:48:30 +0300

    st:modified profile display style

--- awards/urls.py
+++ awards/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.homepage, name='landing'),
     path('profile/<username>/', views.profile, name='profile'),
     path('<username>/profile', views.user_profile, name='userprofile'),
+    path('profile/<username>/settings', views.edit_profile, name='edit'),
 ]

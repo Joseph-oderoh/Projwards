71855c867d42df0395fc6c919b38679f07224353
Joseph_Oderoh <joseph.oderoh@student.moringaschool.com>
2022-06-11 22:09:17 +0300

    st:add forms

---
+++ awards/forms.py
@@ -0,0 +1,18 @@
+
+from django import forms
+
+from awards.models import Profile
+
+
+class UpdateUserForm(forms.ModelForm):
+    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+
+    class Meta:
+        model = User
+        fields = ('username', 'email')
+
+
+class UpdateUserProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['name', 'location', 'profile_picture', 'bio', 'contact']
--- awards/views.py
+++ awards/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from awards.models import AddProjectForm, Project
+from awards.forms import UpdateUserForm, UpdateUserProfileForm
+from awards.models import AddProjectForm, Project, UpdateProfileForm
+from django.contrib.auth.models import User
 # Create your views here.
 
 @login_required(login_url='/accounts/login/')
@@ -19,3 +21,39 @@ def homepage(request):
     else:
             form=AddProjectForm()
     return render(request,'index.html',{'form':form,'projects':project})
+
+
+
+@login_required(login_url='login')
+def profile(request, username):
+    return render(request, 'profile.html')
+
+
+def user_profile(request, username):
+    user_prof = get_object_or_404(User, username=username)
+    if request.user == user_prof:
+        return redirect('profile', username=request.user.username)
+    params = {
+        'user_prof': user_prof,
+    }
+    return render(request, 'userprofile.html', params)
+
+
+@login_required(login_url='login')
+def edit_profile(request, username):
+    user = User.objects.get(username=username)
+    if request.method == 'POST':
+        user_form = UpdateUserForm(request.POST, instance=request.user)
+        prof_form = UpdateProfileForm(request.POST, request.FILES, instance=request.user.profile)
+        if user_form.is_valid() and prof_form.is_valid():
+            user_form.save()
+            prof_form.save()
+            return redirect('profile', user.username)
+    else:
+        user_form = UpdateUserForm(instance=request.user)
+        prof_form = UpdateUserProfileForm(instance=request.user.profile)
+    params = {
+        'user_form': user_form,
+        'prof_form': prof_form
+    }
+    return render(request, 'edit.html', params)
